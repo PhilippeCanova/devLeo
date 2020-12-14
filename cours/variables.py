38c37bb8e929b2ab5528ad4f3388aa4931934efe
PhilippeCanova <canova.philippe@free.fr>
2020-12-14 19:01:44 +0100

    Ajoute le cours sur conditions

--- cours/variables.py
+++ cours/variables.py
@@ -5,7 +5,7 @@ ma_variable = 12
     - ne doivent pas commencer par un chiffre
     - éviter les caractères accuentués
     - commencer par une majuscule pour nommer les classes
-    - utiliserLeCamelCase et utiliser_les_underscores
+    - utiliserLeCamelCase ou utiliser_les_underscores
 """
 
 # Quand une variable est sur la gauche d'un =, on affecte une valeur
@@ -22,15 +22,25 @@ ma_variable = ma_variable * 2
     - les décimaux (float)
     - les chaînes de caractères (string)
     - les bouléens (bool)
+    - None => absence de valeurs
     - les objets propres à Python ou créés par toi
 """
-# On ne peut pas mélanger les types
-# print(message + variable1) => entraine une erreur
 
 # Pour convertir un type, il existe des fonctions
 chaine = str(variable1)
 entier = int(chaine)
-test = bool(0)
+test = bool('')
+print(test)
+
+# On ne peut pas mélanger les types
+# print(message + variable1) => entraine une erreur
+# mais :
+print(message + str(variable1) )
+
+# Pour conntaîre la valeur d'une variable, on l'afficher avec print
+print(chaine)
+print(chaine, entier) # Affiche plusieurs valeurs
+# Mais cela ne donne pas le type
 
 # Pour connaître le type d'une variable, utiliser la fonction type()
 type(entier) # retourne <class 'int'>
@@ -40,3 +50,10 @@ print(type(test)) # retourne <class 'bool'> sur la console
 import decimal
 pi = decimal.Decimal('3.0') #Pi est un objet <class 'decimal.Decimal'>
 print(type(pi))
+
+# Certaines fonctions retournent des valeurs pouvant être récupérées
+# dans des variables
+une_variable = input('Entrer un mot')
+# d'autre s'exécute sans rien retourner (donc en retournant None par défaut)
+une_autre_variable = print("Je retourne None: ")
+print(une_autre_variable) 
